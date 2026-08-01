ae3e31bfa431b08d972ce95c16471975803c5c2c
JavariaRizwan <javariarizwan332@gmail.com>
2026-08-01 15:48:34 +0500

    fix(schema) refine the email regex for proper email submission

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -13,7 +13,7 @@ const userSchema=new mongoose.Schema({
         required:[true, "email is requried"],
         trim:true,
         match: [
-    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,      
+/^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$/,
  "Please enter a valid email",
       ]
     },
